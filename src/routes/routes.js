d2535f29d9536648a39620b2c0aaf55ce2bf3f1b
Checho <ivansolanoquintero.lisurp@gmail.com>
2024-07-29 22:21:47 -0500

    actu by checho

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Login from "../components/login.vue"
 import Home from "../components/home.vue"
 import Administrador from "../components/administrador.vue"
+import Finca from "../components/fincas.vue"
 
 const routes=[
     {path:"/", component:Login},
     {path:"/home", component:Home},
-    {path:"/administrador", component:Administrador}
-
+    {path:"/administrador", component:Administrador},
+    {path:"/finca", component:Finca}
 ]
 
 export const router =createRouter({
