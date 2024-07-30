a683b00ec786ca4ee903d33adbf595b44215030a
elblanquito <SENA@SGLDFPCATSD239.FormacionSena.RED>
2024-07-29 22:16:15 -0500

    home, faltan hacerlo de padre

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,85 +1,220 @@
 <template>
-    <q-layout view="hHh lpR fFf">
-  
-      <q-header elevated class="bg-primary text-white" height-hint="98">
-        <q-toolbar>
-          <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
-  
-          <q-toolbar-title align ="center">
-            <q-avatar>
-              <!-- <img src=""> -->
-            </q-avatar>
-           BUENAS PRACTICAS AGRICULAS
-          </q-toolbar-title>
-        </q-toolbar>
-  
-        <q-tabs align="right">
-          <q-route-tab to="/home" label="Inicio" />
-          <q-route-tab to="/administrador" label="Administrador" />
-          <q-route-tab to="/page3" label="Page Three" />
-        </q-tabs>
-      </q-header>
-  
-      <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title align="center">
+          <q-avatar>
+            <!-- <img src=""> -->
+          </q-avatar>
+          BUENAS PRACTICAS AGRICOLAS
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
       <q-list>
-        <q-item v-for="button in buttons" :key="button.label">
-          <q-btn
-          :label="button.label"
-            color="green"
-            text-color="black"
-            flat
-            class="full-width"
-          />
+        <q-item clickable @click="toggleSubmenu('usuarios')">
+          <q-item-section>
+            <q-btn
+              label="Usuarios"
+              color="green"
+              text-color="black"
+              flat
+              class="full-width"
+            />
+          </q-item-section>
+          <q-item-section avatar>
+            <q-icon :name="submenu.usuarios ? 'expand_less' : 'expand_more'" />
+          </q-item-section>
+        </q-item>
+        <q-item v-if="submenu.usuarios" class="subitem" v-for="item in usuariosItems" :key="item.label">
+          <router-link :to="item.to">
+            <q-item clickable v-ripple class="my-item">
+              <q-item-section avatar>
+                <q-icon :name="item.icon" class="my-icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ item.label }}
+              </q-item-section>
+            </q-item>
+          </router-link>
+        </q-item>
+
+        <q-item clickable @click="toggleSubmenu('agro')">
+          <q-item-section>
+            <q-btn
+              label="Agro"
+              color="green"
+              text-color="black"
+              flat
+              class="full-width"
+            />
+          </q-item-section>
+          <q-item-section avatar>
+            <q-icon :name="submenu.agro ? 'expand_less' : 'expand_more'" />
+          </q-item-section>
+        </q-item>
+        <q-item v-if="submenu.agro" class="subitem" v-for="item in agroItems" :key="item.label">
+          <router-link :to="item.to">
+            <q-item clickable v-ripple class="my-item">
+              <q-item-section avatar>
+                <q-icon :name="item.icon" class="my-icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ item.label }}
+              </q-item-section>
+            </q-item>
+          </router-link>
+        </q-item>
+
+        <q-item clickable @click="toggleSubmenu('gestion')">
+          <q-item-section>
+            <q-btn
+              label="Gestion"
+              color="green"
+              text-color="black"
+              flat
+              class="full-width"
+            />
+          </q-item-section>
+          <q-item-section avatar>
+            <q-icon :name="submenu.gestion ? 'expand_less' : 'expand_more'" />
+          </q-item-section>
+        </q-item>
+        <q-item v-if="submenu.gestion" class="subitem" v-for="item in gestionItems" :key="item.label">
+          <router-link :to="item.to">
+            <q-item clickable v-ripple class="my-item">
+              <q-item-section avatar>
+                <q-icon :name="item.icon" class="my-icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ item.label }}
+              </q-item-section>
+            </q-item>
+          </router-link>
+        </q-item>
+
+        <q-item clickable @click="toggleSubmenu('inventario')">
+          <q-item-section>
+            <q-btn
+              label="Inventario"
+              color="green"
+              text-color="black"
+              flat
+              class="full-width"
+            />
+          </q-item-section>
+          <q-item-section avatar>
+            <q-icon :name="submenu.inventario ? 'expand_less' : 'expand_more'" />
+          </q-item-section>
+        </q-item>
+        <q-item v-if="submenu.inventario" class="subitem" v-for="item in inventarioItems" :key="item.label">
+          <router-link :to="item.to">
+            <q-item clickable v-ripple class="my-item">
+              <q-item-section avatar>
+                <q-icon :name="item.icon" class="my-icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ item.label }}
+              </q-item-section>
+            </q-item>
+          </router-link>
         </q-item>
       </q-list>
     </q-drawer>
-  
-      <q-page-container>
-        <router-view />
-      </q-page-container>
-  
-      <q-footer elevated class="bg-grey-8 text-white">
-        <q-toolbar>
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            <div>Title</div>
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-footer>
-  
-    </q-layout>
-  </template>
-  
-  <script>
-  import { ref } from 'vue'
-  
-  export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const buttons = ref([
-      { label: 'Botón 1' },
-      { label: 'Botón 2' },
-      { label: 'Botón 3' },
-      { label: 'Botón 4' },
-      { label: 'Botón 5' }
-    ])
 
-    return {
-      leftDrawerOpen,
-      buttons,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Title</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const leftDrawerOpen = ref(false)
+const submenu = ref({
+  usuarios: false,
+  agro: false,
+  gestion: false,
+  inventario: false
+})
+
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value
 }
-  </script>
+
+const toggleSubmenu = (menu) => {
+  submenu.value[menu] = !submenu.value[menu]
+}
+
+const usuariosItems = [
+  { label: 'Administrador', to: '/administrador', icon: 'location_city' },
+  { label: 'Empleados', to: '/empleados', icon: 'people' }
+]
+
+const agroItems = [
+  { label: 'Analisis de suelo', to: '/analisissuelo', icon: 'eco' },
+  { label: 'Clima', to: '/clima', icon: 'eco' },
+  { label: 'Control Plagas', to: '/controlplaga', icon: 'eco' },
+  { label: 'Cultivos', to: '/cultivo', icon: 'eco' },
+  { label: 'Elaboracion Sustrato', to: '/elaboracionsustrato', icon: 'eco' },
+  { label: 'Fertilizacion', to: '/fertilizacion', icon: 'eco' },
+  { label: 'Fincas', to: '/finca', icon: 'eco' },
+  { label: 'Parcelas', to: '/parcela', icon: 'eco' },
+  { label: 'Preparacion Suelo', to: '/preparacionsuelo', icon: 'eco' },
+  { label: 'Procesos', to: '/proceso', icon: 'eco' },
+  { label: 'Produccion', to: '/produccion', icon: 'eco' },
+  { label: 'Riego', to: '/riego', icon: 'eco' },
+  { label: 'Siembra', to: '/Siembra', icon: 'eco' }
+]
+
+const gestionItems = [
+  { label: 'Comprador', to: '/comprador', icon: 'business' },
+  { label: 'Factura', to: '/factura', icon: 'business' },
+  { label: 'Gastos', to: '/gastos', icon: 'business' },
+  { label: 'Mantenimientos', to: '/mantenimiento', icon: 'business' },
+  { label: 'Nomina', to: '/nomina', icon: 'business' },
+  { label: 'Proveedores', to: '/proveedor', icon: 'business' }
+]
+
+const inventarioItems = [
+  { label: 'Principal', to: '/inventario1', icon: 'inventory' },
+  { label: 'Semillas', to: '/semilla', icon: 'inventory' },
+  { label: 'Insumos', to: '/insumo', icon: 'inventory' },
+  { label: 'Maquinaria & Herramientas', to: '/maquinariaherramientas', icon: 'inventory' },
+]
+</script>
 
 <style>
 .full-width {
   width: 100%;
   background-color: green;
+}
+
+.subitem {
+  padding-left: 20px;
+  width: 90%;
+}
+
+.my-item {
+  display: flex;
+  align-items: center;
+}
+
+.my-icon {
+  margin-right: 10px;
 }
 </style>
