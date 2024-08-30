ce9e76fa6c20b8175da416e8f6baa0022181dc4b
alexotalora1990 <alexotaloracorredor@gmal.com>
2024-08-30 11:24:51 -0500

    Riego

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,17 +6,14 @@ import Finca from "../components/fincas.vue"
 import Parcela from "../components/parcelas.vue";
 import Empleado from "../components/empleados.vue"
 import Clima from "../components/clima.vue"
-import Proceso from "../components/procesos.vue"
 import Cultivo from "../components/cultivos.vue";
 import Proveedores from '../components/proveedores.vue';
 import Insumos from '../components/insumos.vue'
-
-import Empleado from "../components/empleados.vue"
 import Maquinaria from "../components/maquinaria.vue"
 import Semillas from "../components/semillas.vue"
-
-
 import Proceso from "../components/procesos.vue"
+import Nomina from "../components/nomina.vue"
+import Riego from "../components/riego.vue"
 
 
 
@@ -30,16 +27,18 @@ const routes = [
             { path: "/parcela", component: Parcela},
             { path: "/empleados", component: Empleado},
             { path: "/clima", component: Clima},
-            { path: "/nomina", component: Nomina}
+            { path: "/nomina", component: Nomina},
             { path:"/cultivo", component: Cultivo},
             { path:"/parcela", component: Parcela},
             { path:"/cultivo", component: Cultivo},
-            {path:"/empleados", component: Empleado},
             {path:"/proveedores",component:Proveedores},
             {path:"/insumo",component:Insumos},
             {path:"/maquinaria",component:Maquinaria},
-            {path:"/semilla",component:Semillas}            
-            {path:"/procesos",component:Proceso}
+            {path:"/semilla",component:Semillas},             
+            {path:"/procesos",component:Proceso},
+            {path:"/riegos",component:Riego}
+
+  
 
 
 
