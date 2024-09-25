c479f66a80f7896dd5d13eda525147f3be93ba77
Checho <ivansolanoquintero.lisurp@gmail.com>
2024-09-24 21:51:34 -0500

    cambios

--- src/components/semillas.vue
+++ src/components/semillas.vue
@@ -1,209 +1,107 @@
 <template>
-    <div style="height: 100vh; overflow-y: auto">
-      <div
-        style="
+  <div style="height: 100vh; overflow-y: auto">
+    <div style="
           margin-left: 5%;
           margin-right: 5%;
           display: flex;
           align-items: center;
-        "
-      >
-        <q-btn color="green" class="q-my-md q-ml-md" @click="abrir()"
-          >Crear Semilla</q-btn
-        >
-        <q-select
-          outlined
-          v-model="listar"
-          label="Seleccione"
-          :options="listados"
-          class="q-my-md q-mx-md custom-select"
-        />
-        <q-btn color="black" class="q-my-md q-ml-md" @click="filtrar()"
-          >Filtrar</q-btn
-        >
-      </div>
-      <div>
-        <q-dialog v-model="alert" persistent>
-          <q-card class="" style="width: 700px">
-            <q-card-section
-              style="background-color: #008000; margin-bottom: 20px" class="row items-center"
-            >
-              <div class="text-h6 text-white">
-                {{ accion == 1 ? "Crear Semilla" : "Editar Semilla" }}
-              </div>
-              <q-space/>
-              <q-btn
-                flat
-                dense
-                icon="close"
-                @click="cerrar()"
-                class="text-white"
-              />
-            </q-card-section>
-            <q-select
-              outlined
-              v-model="idproveedores"
-              label="Seleccione un Proveedor"
-              :options="options"
-              class="q-my-md q-mx-md"
-              @filter="filterFn"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="numFactura"
-              label="Número de Factura"
-              class="q-my-md q-mx-md"
-              type="number"
-              :rules="numFacturaRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="fechaCompra"
-              label="Fecha de Compra"
-              class="q-my-md q-mx-md"
-              type="date"
-              :rules="fechaCompraRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="fechaVencimiento"
-              label="Fecha de Vencimiento"
-              class="q-my-md q-mx-md"
-              type="date"
-              :rules="fechaVencimientoRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="especie"
-              label="Especie"
-              class="q-my-md q-mx-md"
-              type="text"
-              :rules="especieRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="NumLote"
-              label="Número de Lote"
-              class="q-my-md q-mx-md"
-              type="number"
-              :rules="NumLoteRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="origen"
-              label="Origen"
-              class="q-my-md q-mx-md"
-              type="text"
-              :rules="origenRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="poderGerminativo"
-              label="Poder Germinativo"
-              class="q-my-md q-mx-md"
-              type="text"
-              :rules="poderGerminativoRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="unidadtotal"
-              label="Unidad Total"
-              class="q-my-md q-mx-md"
-              type="number"
-              :rules="unidadtotalRules"
-              hide-bottom-space
-            />
-            <q-input
-              outlined
-              v-model="total"
-              label="Total"
-              class="q-my-md q-mx-md"
-              type="number"
-              :rules="totalRules"
-              hide-bottom-space
-            />
-  
-            <q-card-actions align="right">
-              <q-btn label="Cerrar" color="black" outline @click="cerrar()" />
-              <q-btn @click="modify()" color="green
-              " class="text-white">
-                {{ accion == 1 ? "Agregar" : "Editar" }}
-              </q-btn>
-            </q-card-actions>
-          </q-card>
-        </q-dialog>
-      </div>
-      <div style="display: flex; justify-content: center">
-        <q-table
-          title="Semillas"
-          title-class="text-green text-weight-bolder text-h5"
-          table-header-class="text-black"
-          :rows="rows"
-          :filter="filter"
-          :columns="columns"
-          row-key="name"
-          style="width: 90%; margin-bottom: 6%"
-          :loading="useSemilla.loading"
-        >
-          <template v-slot:top-right>
-            <q-input color="black" v-model="filter" placeholder="Buscar">
-              <template v-slot:append>
-                <q-icon name="search" />
-              </template>
-            </q-input>
-          </template>
-          <template v-slot:body-cell-estado="props">
-            <q-td :props="props">
-              <template v-slot:loading>
-                <q-spinner
-                  color="primary"
-                  size="100px"
-                  style="align-self: center; margin-bottom: 10px"
-                />
-              </template>
-              <p style="color: green" v-if="props.row.estado == 1">Activo</p>
-              <p style="color: red" v-else>Inactivo</p>
-            </q-td>
-          </template>
-          <template v-slot:body-cell-opciones="props">
-            <q-td :props="props">
-              <q-btn @click="traerDatos(props.row)">
-                <q-tooltip>Editar</q-tooltip>✏️</q-btn
-              >
-              <q-btn @click="desactivar(props.row)" v-if="props.row.estado == 1">
-                <q-tooltip>Desactivar</q-tooltip>❌</q-btn
-              >
-              <q-btn @click="activar(props.row)" v-else>
-                <q-tooltip>Activar</q-tooltip>✅</q-btn
-              >
-            </q-td>
-          </template>
-        </q-table>
-      </div>
+        ">
+      <q-btn color="green" class="q-my-md q-ml-md" @click="abrir()">Crear Semilla</q-btn>
+      <q-select outlined v-model="listar" label="Seleccione" :options="listados"
+        class="q-my-md q-mx-md custom-select" />
+      <q-btn color="black" class="q-my-md q-ml-md" @click="filtrar()">Filtrar</q-btn>
     </div>
-  </template>
+    <div>
+      <q-dialog v-model="alert" persistent>
+        <q-card class="" style="width: 700px">
+          <q-card-section style="background-color: #008000; margin-bottom: 20px" class="row items-center">
+            <div class="text-h6 text-white">
+              {{ accion == 1 ? "Crear Semilla" : "Editar Semilla" }}
+            </div>
+            <q-space />
+            <q-btn flat dense icon="close" @click="cerrar()" class="text-white" />
+          </q-card-section>
+          <q-select outlined v-model="idfinca" label="Seleccione una Finca" :options="options"
+            class="q-my-md q-mx-md" @filter="filterFn" hide-bottom-space />
+          <q-input outlined v-model="nombre" label="Nombre" class="q-my-md q-mx-md" type="text" 
+            hide-bottom-space />
+          <q-input outlined v-model="registroica" label="Registro Ica" class="q-my-md q-mx-md" type="text"
+            hide-bottom-space />
+          <q-input outlined v-model="registroinvima" label="Registro Invima" class="q-my-md q-mx-md" type="text"
+            hide-bottom-space />
+          <q-input outlined v-model="fechaVencimiento" label="Fecha de Vencimiento" class="q-my-md q-mx-md" type="date"
+            hide-bottom-space />
+          <q-input outlined v-model="especie" label="Especie" class="q-my-md q-mx-md" type="text" 
+            hide-bottom-space />
+          <q-input outlined v-model="NumLote" label="Número de Lote" class="q-my-md q-mx-md" type="number"
+            hide-bottom-space />
+          <q-input outlined v-model="origen" label="Origen" class="q-my-md q-mx-md" type="text" 
+            hide-bottom-space />
+          <q-input outlined v-model="poderGerminativo" label="Poder Germinativo" class="q-my-md q-mx-md" type="text"
+            hide-bottom-space />
+          <q-input outlined v-model="observaciones" label="Observaciones" class="q-my-md q-mx-md" type="text"
+            hide-bottom-space />
+          <q-input outlined v-model="cantidad" label="Cantidad" class="q-my-md q-mx-md" type="number"
+            hide-bottom-space />
+
+          <q-card-actions align="right">
+            <q-btn label="Cerrar" color="black" outline @click="cerrar()" />
+            <q-btn @click="modify()" color="green
+              " class="text-white">
+              {{ accion == 1 ? "Agregar" : "Editar" }}
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
+    </div>
+    <div style="display: flex; justify-content: center">
+      <q-table title="Semillas" title-class="text-green text-weight-bolder text-h5" table-header-class="text-black"
+        :rows="rows" :filter="filter" :columns="columns" row-key="name" style="width: 90%; margin-bottom: 6%"
+        :loading="useSemilla.loading">
+        <template v-slot:top-right>
+          <q-input color="black" v-model="filter" placeholder="Buscar">
+            <template v-slot:append>
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </template>
+        <template v-slot:body-cell-estado="props">
+          <q-td :props="props">
+            <template v-slot:loading>
+              <q-spinner color="primary" size="100px" style="align-self: center; margin-bottom: 10px" />
+            </template>
+            <p style="color: green" v-if="props.row.estado == 1">Activo</p>
+            <p style="color: red" v-else>Inactivo</p>
+          </q-td>
+        </template>
+        <template v-slot:body-cell-opciones="props">
+          <q-td :props="props">
+            <q-btn @click="traerDatos(props.row)">
+              <q-tooltip>Editar</q-tooltip>✏️</q-btn>
+            <q-btn @click="desactivar(props.row)" v-if="props.row.estado == 1">
+              <q-tooltip>Desactivar</q-tooltip>❌</q-btn>
+            <q-btn @click="activar(props.row)" v-else>
+              <q-tooltip>Activar</q-tooltip>✅</q-btn>
+          </q-td>
+        </template>
+      </q-table>
+    </div>
+  </div>
+</template>
 
 <script setup>
 import { onMounted, ref } from "vue";
-import { useProveedoresStore } from "../store/proveedores.js";
+import { useFincaStore } from "../store/fincas.js";
 import { useSemillaStore } from "../store/semillas.js";
 import { Notify } from "quasar";
 
-const useProveedor = useProveedoresStore();
+const useFinca = useFincaStore();
 const useSemilla = useSemillaStore();
 
-const filter = ref(""); 
+const filter = ref("");
 
 let rows = ref([]);
-let idproveedores = ref("");
+let idfinca = ref("");
 let numFactura = ref("");
 let fechaCompra = ref("");
 let fechaVencimiento = ref("");
@@ -218,41 +116,7 @@ let id = ref("");
 let alert = ref(false);
 let accion = ref(1);
 
-const isRequired = (msg) => (val) => !!val.trim().length || msg;
-const minLength = (min, msg) => (val) => val.trim().length >= min || msg;
 
-const numFacturaRules = [
-  isRequired("El número de factura es requerido"),
-  minLength(3, "El numero de factura debe tener al menos 3 caracteres"),
-];
-const fechaCompraRules = [
-  isRequired("La fecha de compra es requerida"),
-];
-const fechaVencimientoRules = [
-  isRequired("La fecha de vencimiento es requerida"),
-];
-const especieRules = [
-  isRequired("La especie es requerida"),
-  minLength(3, "La especie debe tener al menos 3 caracteres"),
-];
-    const NumLoteRules = [
-    isRequired("El número de lote es requerido"),
-    (val) => val > 0 || "El número de lote debe ser mayor a 0",
-    ];
-const origenRules = [
-  isRequired("El origen es requerido"),
-];
-const poderGerminativoRules = [
-  isRequired("El poder germinativo es requerido"),
-];
-const unidadtotalRules = [
-  isRequired("La unidad total es requerida"),
-  (val) => val > 0 || "La unidad total debe ser mayor a 0",
-];
-const totalRules = [
-  isRequired("El total es requerido"),
-  (val) => val > 0 || "El total debe ser mayor a 0",
-];
 
 async function crear() {
   if (!validarCampos()) {
@@ -260,16 +124,12 @@ async function crear() {
   }
   try {
     const r = await useSemilla.postSemillas({
-      idproveedores: idproveedores.value.value,
-      numFactura: numFactura.value,
-      fechaCompra: fechaCompra.value,
+      idfinca: idfinca.value.value,
       fechaVencimiento: fechaVencimiento.value,
       especie: especie.value,
       NumLote: NumLote.value,
       origen: origen.value,
       poderGerminativo: poderGerminativo.value,
-      unidadtotal: unidadtotal.value,
-      total: total.value,
     });
     Notify.create({
       message: "Semilla creada correctamente!",
@@ -292,10 +152,10 @@ async function crear() {
 function traerDatos(semilla) {
   alert.value = true;
   accion.value = 2;
-  idproveedores.value = {
-  label: semilla.idproveedores.nombre,
-  value: semilla.idproveedores._id,
-};
+  idfinca.value = {
+    label: semilla.idfinca.nombre,
+    value: semilla.idfinca._id,
+  };
   numFactura.value = semilla.numFactura;
   fechaCompra.value = semilla.fechaCompra;
   fechaVencimiento.value = semilla.fechaVencimiento;
@@ -315,7 +175,7 @@ async function editar() {
   }
   try {
     const r = await useSemilla.putSemillas(id.value, {
-      idproveedores: idproveedores.value.value,
+      idfinca: idfinca.value.value,
       numFactura: numFactura.value,
       fechaCompra: fechaCompra.value,
       fechaVencimiento: fechaVencimiento.value,
@@ -351,28 +211,28 @@ function modify() {
   }
 }
 
-let proveedores = [];
+let fincas = [];
 let datos = {};
-let options = ref(proveedores);
+let options = ref(fincas);
 
 function filterFn(val, update, abort) {
   update(() => {
     const needle = val.toLowerCase();
-    options.value = proveedores.filter(v => v.label.toLowerCase().indexOf(needle) > -1);
+    options.value = fincas.filter(v => v.label.toLowerCase().indexOf(needle) > -1);
   })
 }
 
 
-async function listarProveedores() {
-  const data = await useProveedor.getProveedoresActivos();
-  data.data.proveedores.forEach((item) => {
+async function listarFincas() {
+  const data = await useFinca.getFincasActivos();
+  data.data.fincaActiva.forEach((item) => {
     datos = {
       label: item.nombre,
       value: item._id,
     };
-    proveedores.push(datos);
+    fincas.push(datos);
   });
-  console.log(proveedores);
+  console.log(fincas);
 }
 
 
@@ -435,28 +295,28 @@ async function activar(semilla) {
 }
 
 const columns = ref([
- {
-  name: "idproveedores",
-  required: true,
-  label: "Proveedor",
-  align: "center",
-  field: (row) => row.idproveedores.nombre,
-  sortable: true,
-},
   {
-    name: "numFactura",
+    name: "nombre",
     required: true,
-    label: "Número de Factura",
+    label: "Semilla",
     align: "center",
-    field: "numFactura",
+    field: "nombre",
     sortable: true,
   },
   {
-    name: "fechaCompra",
+    name: "ICA",
     required: true,
-    label: "Fecha de Compra",
+    label: "ICA",
     align: "center",
-    field: "fechaCompra",
+    field: "registroica",
+    sortable: true,
+  },
+  {
+    name: "INVIMA",
+    required: true,
+    label: "INVIMA",
+    align: "center",
+    field: "registroinvima",
     sortable: true,
   },
   {
@@ -466,6 +326,9 @@ const columns = ref([
     align: "center",
     field: "fechaVencimiento",
     sortable: true,
+    format: (val) => {
+      return val.split('T')[0].split('-').reverse().join('/');
+    }
   },
   {
     name: "especie",
@@ -500,19 +363,19 @@ const columns = ref([
     sortable: true,
   },
   {
-    name: "unidadtotal",
+    name: "Cantidad",
     required: true,
-    label: "Unidad Total",
+    label: "Cantidad",
     align: "center",
-    field: "unidadtotal",
+    field: "cantidad",
     sortable: true,
   },
   {
-    name: "total",
+    name: "observaciones",
     required: true,
-    label: "Total",
+    label: "Observaciones",
     align: "center",
-    field: "total",
+    field: "observaciones",
     sortable: true,
   },
   {
@@ -545,7 +408,7 @@ function cerrar() {
 }
 
 function limpiarCampos() {
-  idproveedores.value = "";
+  idfinca.value = "";
   numFactura.value = "";
   fechaCompra.value = "";
   fechaVencimiento.value = "";
@@ -560,7 +423,7 @@ function limpiarCampos() {
 
 function validarCampos() {
   if (
-    !idproveedores.value ||
+    !idfinca.value ||
     !numFactura.value ||
     !fechaCompra.value ||
     !fechaVencimiento.value ||
@@ -582,7 +445,7 @@ function validarCampos() {
 }
 
 onMounted(async () => {
-  listarProveedores();
+  listarFincas();
   listarSemillas();
 });
 
@@ -591,11 +454,11 @@ onMounted(async () => {
 
 <style>
 .custom-select {
-    font-size: 14px;
-    border-radius: 4px;
-    padding: 8px 12px;
-    background-color: white;
-    color: #333;
-    min-width: 200px;
+  font-size: 14px;
+  border-radius: 4px;
+  padding: 8px 12px;
+  background-color: white;
+  color: #333;
+  min-width: 200px;
 }
 </style>
